bf11ff41412b6ab413612299bbc7f0c5f0bc7cb8
ashkotian84 <ash.karkera@gmail.com>
2019-11-28 19:17:08 +0530

    initial commit

---
+++ payment-backend/src/main/java/com/example/demo/PaymentBackendApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class PaymentBackendApplication extends SpringBootServletInitializer {
+
+	private static Class applicationClass = PaymentBackendApplication.class;
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentBackendApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+}
